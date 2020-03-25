1645915a5be0a052aa37047ab65e64cb3bc9bc4b
Chiamaka Ezeoke <aezeokeamaka@gmail.com>
2020-03-25 11:01:19 +0100

    Code to print name, courses and even numbers

---
+++ Task-1.js
@@ -0,0 +1,16 @@
+//Javascript Task 1
+var name = "Ezeoke Chiamaka";
+var courses = ["HTML", "CSS", "Javascript", "UI/UX"];
+
+console.log("Name: " + name);
+console.log("Courses: " + courses);
+
+num = courses.length;
+console.log(num + " is an even number");
+
+for (i = 0; i <= 200; i++){
+    if(i % 2 == 0){
+        console.log(i);
+    }
+}
+
